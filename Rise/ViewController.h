461833a221b7ba7ef5071eaf60c0eedacd70b330
Kevin Sullivan <sideslapd@gmail.com>
2014-06-20 20:50:36 -0400

    Initial Commit

---
+++ Rise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rise
+//
+//  Created by Kevin Sullivan on 6/20/14.
+//  Copyright (c) 2014 com.sideapps.rise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
